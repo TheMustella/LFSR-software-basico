868fa16ff9385b4b2df247f095c01db485059020
Jonas Prado <themustellafuro@gmail.com>
2017-05-05 17:33:55 -0300

    Update arquivo.c

--- arquivo.c
+++ arquivo.c
@@ -1,2 +1,25 @@
 //LFSR-software-basico
 //Linear-feedback_shift_register de Software Basico (1/2017)
+
+#include <stdio.h>
+#include <string.h>
+
+/* define simple structure */
+struct {
+   unsigned int widthValidated;
+   unsigned int heightValidated;
+} status1;
+
+/* define a structure with bit fields */
+struct {
+   unsigned int widthValidated : 1;
+   unsigned int heightValidated : 1;
+} status2;
+ 
+int main( ) {
+
+   printf( "Memory size occupied by status1 : %d\n", sizeof(status1));
+   printf( "Memory size occupied by status2 : %d\n", sizeof(status2));
+
+   return 0;
+}
